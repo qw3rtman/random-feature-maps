63807eb3f3c708c771e4eb177d2034c8c64957f7
Tianshu Huang <thetianshuhuang@gmail.com>
2018-12-09 22:41:29 -0600

    Added color histogram to rbfsvm

--- rbfsvm.py
+++ rbfsvm.py
@@ -5,6 +5,7 @@ import idc
 from classify import ClassifyTest
 from rbf import RandomBinningFeature
 import pinfo
+from ckm import get_idc_colors
 
 
 def train(dataset):
@@ -16,17 +17,18 @@ def train(dataset):
     return rfsvm
 
 
-def run(ptrain=0.01, ptest=0.1, fdim=10000, ntrain=-25, ntest=25):
+def run(ptrain=0.01, ptest=0.1, fdim=10000, ntrain=-25, ntest=25, n=20):
 
     timer = pinfo.Task("Random Fourier Feature Support Vector Classifier")
     rbf = RandomBinningFeature(7500, int(fdim))
+    ckm = get_idc_colors(int(n))
 
     dataset = idc.IDCDataset(
         idc.PATIENTS[:int(ntrain)],
-        p=float(ptrain), transform=rbf.transform)
+        p=float(ptrain), feature=ckm.map, transform=rbf.transform)
     test_dataset = idc.IDCDataset(
         idc.PATIENTS[-int(ntest):],
-        p=float(ptest), transform=rbf.transform)
+        p=float(ptest), feature=ckm.map, transform=rbf.transform)
     tester = ClassifyTest(test_dataset.data, test_dataset.classes)
 
     rfsvm = train(dataset)
